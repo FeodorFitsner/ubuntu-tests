b863efdfad72102cbaf9b0182ffd429c91df01ef
Feodor Fitsner <feodor@appveyor.com>
2017-04-27 11:28:48 -0700

    Push-AppveyorArtifact test.js

--- build-agent-api.ps1
+++ build-agent-api.ps1
@@ -16,4 +16,41 @@ function Update-AppveyorBuild() {
     $response = Invoke-WebRequest -Method PUT -Uri $env:APPVEYOR_API_URL/api/build -Headers $headers -Body (ConvertTo-Json $body -Depth 6)
 }
 
+function Push-AppveyorArtifact() {
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Position=0, Mandatory=$true)]
+        $Path,
+
+        [Parameter(Mandatory=$false)]
+        $FileName = $null,
+
+        [Parameter(Mandatory=$false)]
+        $DeploymentName = $null,
+
+        [Parameter(Mandatory=$false)]
+        $Type = $null,
+
+        [Parameter(Mandatory=$false)]
+        $Verbosity = 'Normal'
+    )
+
+    $fullPath = (Resolve-Path $Path).Path
+    
+    $headers = @{
+      "Content-type" = "application/json"
+    }
+    
+    $body = @{
+      "path" = $fullPath
+      "fileName" = $FileName
+      "name" = $DeploymentName
+      "type" = $Type
+    }
+    
+    $response = Invoke-WebRequest -Method POST -Uri $env:APPVEYOR_API_URL/api/artifacts -Headers $headers -Body (ConvertTo-Json $body -Depth 6)
+}
+
 Update-AppveyorBuild -Version 1.2.$env:APPVEYOR_BUILD_NUMBER-abc
+Push-AppveyorArtifact test.js
