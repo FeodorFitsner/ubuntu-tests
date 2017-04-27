0d877014508a4b90a3e1124a2e5c949161392852
Feodor Fitsner <feodor@appveyor.com>
2017-04-27 11:30:33 -0700

    pwd

--- build-agent-api.ps1
+++ build-agent-api.ps1
@@ -52,5 +52,6 @@ function Push-AppveyorArtifact() {
     $response = Invoke-WebRequest -Method POST -Uri $env:APPVEYOR_API_URL/api/artifacts -Headers $headers -Body (ConvertTo-Json $body -Depth 6)
 }
 
+pwd
 Update-AppveyorBuild -Version 1.2.$env:APPVEYOR_BUILD_NUMBER-abc
 Push-AppveyorArtifact test.js
