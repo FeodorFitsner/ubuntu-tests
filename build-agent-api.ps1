9b3e0f61c36db2a1cca34379bfac03a08303ecdf
Feodor Fitsner <feodor@appveyor.com>
2017-04-27 10:53:28 -0700

    Create build-agent-api.ps1

---
+++ build-agent-api.ps1
@@ -0,0 +1,17 @@
+function Update-AppveyorBuild() {
+    [CmdletBinding()] 
+    Param( 
+        [Parameter(Mandatory=$false)] 
+        [string]$Version
+    )
+    
+    $headers = {
+      "Content-type" = "application/json"
+    }
+    
+    $body = {
+      "version" = $Version
+    }
+    
+    Invoke-WebRequest -Method PUT -Uri $env:APPVEYOR_API_URL/api/build -Headers $headers -Body (ConvertTo-Json $body -Depth 6)
+}
