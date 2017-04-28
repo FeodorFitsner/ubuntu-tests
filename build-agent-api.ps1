2ad46957e9e59225061246832be136ac788d4d30
Feodor Fitsner <feodor@appveyor.com>
2017-04-28 11:11:36 -0700

    $respBody.lockPath

--- build-agent-api.ps1
+++ build-agent-api.ps1
@@ -51,7 +51,8 @@ function Push-AppveyorArtifact() {
     }
     
     $response = Invoke-WebRequest -Method POST -Uri $env:APPVEYOR_API_URL/api/artifacts -Headers $headers -Body (ConvertTo-Json $body -Depth 6)
-    $response
+    $respBody = ConvertFrom-Json $response.content
+    $respBody.lockPath
 }
 
 Update-AppveyorBuild -Version 1.2.$env:APPVEYOR_BUILD_NUMBER-abc
